ce40fcc4429779f6c2d1d02a1a86b7356d1ef813
Koli14 <koli14@gmail.com>
2021-03-03 14:22:43 +0100

    add CRUD functionality with npm packige

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,5 +1,13 @@
+import crud, { sequelizeCrud } from 'express-sequelize-crud'
+import model from '../models'
+
+const { Partner, Settlement, CompanyType } = model
 
 export default async (app) => {
+  app.use(crud('/admin/partners', sequelizeCrud(Partner)))
+  app.use(crud('/admin/settlements', sequelizeCrud(Settlement)))
+  app.use(crud('/admin/companyTypes', sequelizeCrud(CompanyType)))
+
   app.all('*', (req, res) => res.status(200).send({
     message: 'Hello World!'
   }))
